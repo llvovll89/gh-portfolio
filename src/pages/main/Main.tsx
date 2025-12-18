38a70e33a55e4ceda4e4a6a9b020554959314366
llvovll89 <llvovll89@igis.co.kr>
2025-12-18 11:27:51 +0900

    feat: 아무런 folder도 선택되지 않았을 경우 보여줄 contents 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,21 +1,29 @@
+import {useContext} from "react";
 import {Aside} from "../../components/aside/Aside";
-import { Contents } from "../../components/contents/Contents";
+import {Contents} from "../../components/contents/Contents";
 import {Bottom} from "../../components/footer/Footer";
 import {Header} from "../../components/header/Header";
+import {GlobalStateContext} from "../../context/GlobalState.context";
 
 export const Main = () => {
+    const {selectedPathState} = useContext(GlobalStateContext);
+
     return (
         <>
             <Header />
             <Aside />
-            <Contents className="select-none">
-                <section className="w-full h-full bg-white py-4 px-5 flex flex-col items-center">
-                    <header className=""></header>
-                    <article className="flex flex-col w-full">
-                        <h1 className="text-2xl font-bold w-full flex justify-start">Web Developer "김건호"</h1>
-                    </article>
-                </section>
-            </Contents>
+            {selectedPathState.list.length > 0 && (
+                <Contents className="select-none">
+                    <section className="w-full h-full bg-white py-4 px-5 flex flex-col items-center">
+                        <header className=""></header>
+                        <article className="flex flex-col w-full">
+                            <h1 className="text-2xl font-bold w-full flex justify-start">
+                                Web Developer "김건호"
+                            </h1>
+                        </article>
+                    </section>
+                </Contents>
+            )}
             <Bottom />
         </>
     );
